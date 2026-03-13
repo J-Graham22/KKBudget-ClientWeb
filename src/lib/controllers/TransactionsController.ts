fc9fc7434a6da855f87d7134ce96b35afc0954c3
Justin Graham <justin.m.graham2@gmail.com>
2026-03-12 23:33:28 -0400

    added transactions controller

---
+++ src/lib/controllers/TransactionsController.ts
@@ -0,0 +1,9 @@
+const getAllTransactions = (householdId: number) => {
+
+}
+
+const getTransactionsByBudget = (budgetId: number) => {
+
+}
+
+// perhaps set it up so the endpoint has a number of parameters that determine it's output?
