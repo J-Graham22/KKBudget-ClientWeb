479be877e57cf22dc23e370c1d1d868f6f10af5a
Justin Graham <justin.m.graham2@gmail.com>
2026-03-22 16:19:55 -0400

    started adding for onboarding flow

---
+++ src/routes/onboarding.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    type Step = 
+        | "welcome"
+        | "user"
+        | "household"
+        | "accounts"
+        | "firstBudget"
+        | "categories"
+        | "otherUsers"
+        | "complete";
+
+    
+
+</script>
+
+<div>
+
+</div>
+
+<style>
+
+</style>
