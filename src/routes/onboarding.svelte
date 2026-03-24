e33b985d0bae8a1da82821f14311e89208f16922
Justin Graham <justin.m.graham2@gmail.com>
2026-03-23 23:09:46 -0400

    further progress on onboarding flow

--- src/routes/onboarding.svelte
+++ src/routes/onboarding.svelte
@@ -1,4 +1,13 @@
 <script lang="ts">
+    import AccountStep from "$lib/components/onboarding/AccountStep.svelte";
+    import BudgetStep from "$lib/components/onboarding/BudgetStep.svelte";
+    import CategoriesStep from "$lib/components/onboarding/CategoriesStep.svelte";
+    import CompleteStep from "$lib/components/onboarding/CompleteStep.svelte";
+    import ProfileStep from "$lib/components/onboarding/ProfileStep.svelte";
+    import WelcomeStep from "$lib/components/onboarding/WelcomeStep.svelte";
+
+    interface onboardingData {}
+
     type Step = 
         | "welcome"
         | "user"
@@ -8,8 +17,42 @@
         | "categories"
         | "otherUsers"
         | "complete";
-
     
+    let currentStep: Step = "welcome";
+
+    let onboarding: onboardingData = "";
+
+    const next = () => {
+        const order: Step[] = [
+            "welcome",
+            "user",
+            "household",
+            "accounts",
+            "firstBudget",
+            "categories",
+            "otherUsers",
+            "complete"
+        ];
+
+        const index = order.indexOf(currentStep);
+        currentStep = order[index + 1];
+    }
+
+    const back = () => {
+        const order: Step[] = [
+            "welcome",
+            "user",
+            "household",
+            "accounts",
+            "firstBudget",
+            "categories",
+            "otherUsers",
+            "complete"
+        ];
+
+        const index = order.indexOf(currentStep);
+        currentStep = order[index - 1];
+    }
 
 </script>
 
